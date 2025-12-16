cda79a222dc5642fdebddbae655d68579b5986d6
thebananasmx <hector@thebananas.com.mx>
2025-12-15 21:34:03 -0600

    chore: Update dependencies and Vite config
    Upgrades React, i18next, and related dependencies to their latest versions. Also includes an optimization to include i18next related packages in Vite's dependency pre-bundling for improved performance.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-i18next', 'i18next', 'i18next-browser-languagedetector']
+  }
 })
